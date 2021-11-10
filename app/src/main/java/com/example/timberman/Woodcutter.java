85bb069784bfd8caa06ba6735f683a6bbd1b3454
leman789 <leman3211@wp.pl>
2021-11-10 17:27:22 +0100

    Create Woodcutter.java

---
+++ app/src/main/java/com/example/timberman/Woodcutter.java
@@ -0,0 +1,34 @@
+package com.example.timberman;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.ArrayList;
+
+//drwal
+public class Woodcutter extends BaseObject {
+    private ArrayList<Bitmap> arrBms = new ArrayList<>();
+    public Woodcutter(){
+
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(this.getBm(),this.x, this.y, null);
+    }
+
+    public ArrayList<Bitmap> getArrBms() {
+        return arrBms;
+    }
+
+    public void setArrBms(ArrayList<Bitmap> arrBms) {
+        this.arrBms = arrBms;
+        for (int i = 0; i<arrBms.size(); i++){
+            this.arrBms.set(i,Bitmap.createScaledBitmap(this.arrBms.get(i),this.width,this.height, true));
+        }
+    }
+
+    @Override
+    public Bitmap getBm() {
+        return this.getArrBms().get(0);
+    }
+}
