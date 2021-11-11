e50657a4707a93b6298895fa316a6dc8028ce56f
leman789 <leman3211@wp.pl>
2021-11-11 18:29:52 +0100

    Update GameView.java

--- app/src/main/java/com/example/timberman/GameView.java
+++ app/src/main/java/com/example/timberman/GameView.java
@@ -5,15 +5,22 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.logging.LogRecord;
 
 public class GameView extends View {
     private Woodcutter woodcutter;
+    private android.os.Handler handler;
+    private Runnable r;
+    private float xValue, yValue,leftPersentage;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         woodcutter = new Woodcutter();
@@ -27,9 +34,32 @@ public class GameView extends View {
         arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.woodcutter));
         arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.woodcutter2));
         woodcutter.setArrBms(arrBms);
+
+        leftPersentage = (Constants.SCREEN_WIDTH)*50/100;
+        handler = new Handler();
+        r = new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+            }
+        };
+
     }
     public  void draw(Canvas canvas){
         super.draw(canvas);
         woodcutter.draw(canvas);
+        handler.postDelayed(r, 10);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            xValue = event.getX();
+            yValue = event.getY();
+            if(xValue <= leftPersentage){
+                woodcutter.onClick();
+            }
+        }
+        return true;
     }
 }
