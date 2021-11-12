609a54be21a9732d5ff36578462c0dc9dc1bcf7c
r3czus <76210775+r3czus@users.noreply.github.com>
2021-11-12 12:23:24 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/timberman/MainActivity.java
+++ app/src/main/java/com/example/timberman/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static RelativeLayout rl_game_over;
     private  GameView gv;
+    public static ImageView imageView_logo;
     public static ImageButton btn_start,btn_shop,btn_info;
 
 
@@ -25,10 +29,37 @@ public class MainActivity extends AppCompatActivity {
         Constants.SCREEN_WIDTH = dm.widthPixels;
         Constants.SCREEN_HEIGHT = dm.heightPixels;
         setContentView(R.layout.activity_main);
+        imageView_logo=findViewById(R.id.imageView_logo);
         btn_start=findViewById(R.id.btn_start);
         btn_info=findViewById(R.id.btn_info);
         btn_shop=findViewById(R.id.btn_shop);
         gv=findViewById(R.id.gv);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gv.setStart(true);
+                //w tymi miejscu trzeba dopisać schowanie punkotw
+                btn_start.setVisibility(view.INVISIBLE);
+                btn_info.setVisibility(view.INVISIBLE);
+                btn_shop.setVisibility(view.INVISIBLE);
+                imageView_logo.setVisibility(view.INVISIBLE);
+
+            }
+        });
+        btn_shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,R.string.shop_string
+                        , Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        R.string.info_string, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
