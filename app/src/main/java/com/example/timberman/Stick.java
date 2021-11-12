252b6cb21a0c1e676c7cc7bfaee4ccca3166e3a9
leman789 <leman3211@wp.pl>
2021-11-12 19:07:31 +0100

    Update Stick.java

--- app/src/main/java/com/example/timberman/Stick.java
+++ app/src/main/java/com/example/timberman/Stick.java
@@ -4,20 +4,18 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 public class Stick extends BaseObject{
-    public static int speed;
+    private  int idCurrentBitmap;
 
     public Stick(float x, float y, int width, int height) {
         super(x, y, width, height);
-        speed=10*Constants.SCREEN_HEIGHT/1920;
     }
     public void draw(Canvas canvas){
-        this.y-=speed;
-        canvas.drawBitmap(this.bm,this.x,this.y,null);
+        canvas.drawBitmap(this.getBm(),this.x,this.y,null);
     }
 
     @Override
-    public void setBm(Bitmap bm) {
-        this.bm = Bitmap.createScaledBitmap(bm,width,height,true);
+    public void setBm(Bitmap Bm) {
+      this.bm =  Bitmap.createScaledBitmap(Bm,this.width,this.height, true);
     }
 }
 
