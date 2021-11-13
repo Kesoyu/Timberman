0d150324fa2094a6ff290f88e75fc2d317848995
r3czus <76210775+r3czus@users.noreply.github.com>
2021-11-13 20:42:59 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/timberman/MainActivity.java
+++ app/src/main/java/com/example/timberman/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.timberman;
 
+import static com.example.timberman.R.id.btn_shop_back;
+import static com.example.timberman.R.id.start;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -15,10 +21,12 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     public static RelativeLayout rl_game_over;
     private  GameView gv;
-    public static ImageView imageView_logo;
-    public static ImageButton btn_start,btn_shop,btn_info;
+    public static ImageView imageView_logo,imageView_timber_man;
+    public static ImageButton btn_start,btn_shop,btn_info,btn_pause,btn_select;
 
 
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +38,13 @@ public class MainActivity extends AppCompatActivity {
         Constants.SCREEN_HEIGHT = dm.heightPixels;
         setContentView(R.layout.activity_main);
         imageView_logo=findViewById(R.id.imageView_logo);
+        btn_pause=findViewById(R.id.btn_pause);
         btn_start=findViewById(R.id.btn_start);
         btn_info=findViewById(R.id.btn_info);
         btn_shop=findViewById(R.id.btn_shop);
+        btn_select=findViewById(R.id.btn_select);
         gv=findViewById(R.id.gv);
+
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 btn_start.setVisibility(view.INVISIBLE);
                 btn_info.setVisibility(view.INVISIBLE);
                 btn_shop.setVisibility(view.INVISIBLE);
+                btn_pause.setVisibility(view.VISIBLE);
                 imageView_logo.setVisibility(view.INVISIBLE);
 
             }
@@ -49,8 +61,9 @@ public class MainActivity extends AppCompatActivity {
         btn_shop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,R.string.shop_string
-                        , Toast.LENGTH_LONG).show();
+                Intent intent =new Intent(MainActivity.this,ShopActivity.class);
+                startActivity(intent);
+
             }
         });
         btn_info.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +73,24 @@ public class MainActivity extends AppCompatActivity {
                         R.string.info_string, Toast.LENGTH_LONG).show();
             }
         });
+        btn_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        R.string.pause_string, Toast.LENGTH_LONG).show();
+
+            }
+        });
+        btn_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_start.setVisibility(view.VISIBLE);
+                btn_info.setVisibility(view.VISIBLE);
+                btn_shop.setVisibility(view.VISIBLE);
+                btn_select.setVisibility(view.INVISIBLE);
+            }
+        });
+
 
     }
 }
