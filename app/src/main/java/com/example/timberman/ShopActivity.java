70769f278e08965bd81f51503d117d0a7fed8aba
r3czus <76210775+r3czus@users.noreply.github.com>
2022-01-06 19:17:01 +0100

    lekutka poprawa sklepu
    ..

--- app/src/main/java/com/example/timberman/ShopActivity.java
+++ app/src/main/java/com/example/timberman/ShopActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -11,28 +12,75 @@ import android.widget.ImageView;
 
 public class ShopActivity extends AppCompatActivity {
     public ImageButton Avatar1;
-    public ImageView Avatar2,Avatar3,Avatar4;
-    public Button btn_shop_back;
+    public ImageView Avatar2,Avatar3,Avatar4,woodercutter1,popcat2;
+    public Button btn_shop_back,btn_next,btn_previous;
+    public int avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
-        Avatar1 = findViewById(R.id.Avatar1);
+
+
+       // Avatar1 = findViewById(R.id.Avatar1);
 
         if(Constants.bestScore>10){
-            Avatar1.setImageResource(R.drawable.shop);
+          //  Avatar1.setImageResource(R.drawable.shop);
         }
         btn_shop_back=findViewById(R.id.btn_shop_back);
+        btn_next=findViewById(R.id.btn_next);
+        btn_previous=findViewById(R.id.btn_previous);
+        woodercutter1=findViewById(R.id.woodercutter1);
+        popcat2=findViewById(R.id.popcat2);
+        avatar=Constants.Avatar;
+        WhoChose(avatar);
+
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                avatar++;
+                if(avatar>1)
+                    avatar=0;
+                WhoChose(avatar);
+            }
+        });
+
+        btn_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                avatar--;
+                if(avatar==-1)
+                    avatar=1;
+                WhoChose(avatar);
+            }
+        });
+
+
+
         btn_shop_back.setOnClickListener(view -> {
             Constants.Restart=false;
             Intent intent= new Intent(ShopActivity.this,MainActivity.class);
+            Constants.Avatar=avatar;
             startActivity(intent);
         });
-        Avatar1.setOnClickListener(v -> {
-            if(Constants.bestScore>10){
-               Constants.Avatar=1;
-            }
-        });
+//        Avatar1.setOnClickListener(v -> {
+//            if(Constants.bestScore>10){
+//               Constants.Avatar=1;
+//            }
+//        });
+
+    }
+    void WhoChose(int avatar)
+    {
+        if(avatar==0) {
+            woodercutter1.setVisibility(View.VISIBLE);
+            popcat2.setVisibility(View.INVISIBLE);
+        }
+
+        else if (avatar==1) {
+            popcat2.setVisibility(View.VISIBLE);
+            woodercutter1.setVisibility(View.INVISIBLE);
+        }
     }
 }
