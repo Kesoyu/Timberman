f294eea4b7f38193ebccf82c98f2a3ed72333318
leman789 <leman3211@wp.pl>
2022-01-12 16:37:40 +0100

    Update GameView.java

--- app/src/main/java/com/example/timberman/GameView.java
+++ app/src/main/java/com/example/timberman/GameView.java
@@ -178,6 +178,7 @@ public class GameView extends View {
         canvas.drawBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.podstawadrzewauseless),(478*Constants.SCREEN_WIDTH/1080),(100*Constants.SCREEN_HEIGHT/1920), true),(322*Constants.SCREEN_WIDTH/1080),(1540*Constants.SCREEN_HEIGHT/1920), null);
             woodcutter.draw(canvas);
         if(Constants.IsDead){ //todo tu bedzie else do zrestartowania gry
+            start=false;
             woodcutter.smierc();
             for(int i = 0;i<arrSicks.size(); i++){
                 arrSicks.get(i).draw(canvas);
